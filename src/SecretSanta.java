1a607932017edc5923a899d8366402c86a3f4317
George Beuhler <gebeuhler@gmail.com>
2014-10-29 20:35:25 -0500

    added unit tests. re-added helper method

--- src/SecretSanta.java
+++ src/SecretSanta.java
@@ -5,17 +5,30 @@ public class SecretSanta{
 	/**
 	 * @param participants list of individuals participating in the draw.
 	 * @return the list of individuals paired with the participants where participants[0] is matched
-	 * with assignments[0], participants[1] is matched with assignments[1], 
-	 *	and so on.
+     * with assignments[0], participants[1] is matched with assignments[1], and so on.
 	 */
 	public String[] generateAssignments(final String[] participants){
 		final String[] assignments = new String[participants.length];
 		System.arraycopy(participants, 0, assignments, 0, assignments.length);
 
-        do {
+		do {
             Collections.shuffle(Arrays.asList(assignments));
-        } while(Arrays.equals(participants,assignments));
+            } while(!hasSelfAssignment(participants, assignments));
 
 		return assignments;
+	}
+
+	/**
+	 * @param list of participants
+	 * @param list of assignments
+	 * @return false if a person is assigned to himself and the array must be reshuffled;true otherwise
+	 */
+	private boolean hasSelfAssignment(String[] participants, String[] assignments){
+		for(int i = 0; i < participants.length; i++){
+			if(participants[i].equals(assignments[i])) {
+                return false;
+            }
+		}
+		return true;
 	}
 }
--- test/TestSecretSanta.java
+++ test/TestSecretSanta.java
@@ -1,25 +1,44 @@
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.*;
 
-import java.util.Arrays;
-
 public class TestSecretSanta{
 
-    final String[] participants = new String[] { "Kyle", "Kenny", "Eric", "Stan", "Stewie", "Brian" };
-    SecretSanta secretSanta = new SecretSanta();
+    String[] participants;
+    SecretSanta secretSanta;
+
+    /**
+     * Initialize testing variables
+     */
+    @Before
+    public void initialize(){
+       participants = new String[] { "Kyle", "Kenny", "Eric", "Stan", "Stewie", "Brian" };
+       secretSanta = new SecretSanta();
+    }
 
     /**
      * Test one randomized set of participants
      */
 	@Test
-	public void testGeneration(){
+	public void testSelfAssignment(){
         final String[] assignments = secretSanta.generateAssignments(participants);
-        assertFalse(Arrays.equals(participants,assignments));
+        boolean hasSelfAssignment = false;
+        for(int i = 0; i < participants.length; i++){
+            if(participants[i].equals(assignments[i])) {
+                hasSelfAssignment = true;
+            }
+        }
+        assertFalse(hasSelfAssignment);
 	}
 
-    @Test
-    public void testSelfAssignment(){
-
+    /**
+     * Test that every person has an assignment. This is done by
+     * verifying the input and output arrays have equal length.
+     */
+   @Test
+   public void testEqualLength(){
+       final String[] assignments = secretSanta.generateAssignments(participants);
+       assertTrue(assignments.length == participants.length);
     }
 }
